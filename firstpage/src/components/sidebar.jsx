67c09d9b7eb091c6850b728ba540a60f8ebeb20e
Sanidhya Rathore <sanidhya.rathore263@gmail.com>
2024-12-20 00:35:10 +0530

    added divider

--- firstpage/src/components/sidebar.jsx
+++ firstpage/src/components/sidebar.jsx
@@ -3,11 +3,14 @@ import image from "../assets/logo.svg";
 const Sidebar = () => {
     return (
         <>
-            <div className="bg-[#115E56] justify-items-center w-fit min-w-[60px] h-[1080px] pt-6 pr-4 pb-6 pl-4" >
-                <img src={image} />
+            <div className="bg-[#115E56] justify-items-center w-fit min-w-[60px] h-[1080px]">
+                <div className="  pt-6 pr-4 pb-6 pl-4" >
+                    <img src={image} />
+                </div>
+                <div className="h-px bg-[#134E48] min-w-[34px]">
+
+                </div>
             </div>
-
-
 
         </>
     )
