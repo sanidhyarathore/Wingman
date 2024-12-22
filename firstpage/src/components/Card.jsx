5ffe4ad65524245e678e5955bfbcfcd9e1823cfa
Sanidhya Rathore <sanidhya.rathore263@gmail.com>
2024-12-22 21:37:29 +0530

    edits in card

--- firstpage/src/components/Card.jsx
+++ firstpage/src/components/Card.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Card() {
     return (
-        <div className='p-6  w-max-[396px] w-fill bg-red-200'>hhhhhh</div>
+        <div className='p-6  w-max-[396px] w-fill bg-red-200'>
+            <div className=''>this is title</div>
+            <div className=''>this is amount</div>
+            <div className=''>this is increase or decrease</div>
+        </div>
     )
 }
 
