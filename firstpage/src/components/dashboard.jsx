1267954e562b79b86489b6014fa5c79bf186cd6d
Sanidhya Rathore <sanidhya.rathore263@gmail.com>
2025-01-08 23:56:30 +0530

    minor change

--- firstpage/src/components/dashboard.jsx
+++ firstpage/src/components/dashboard.jsx
@@ -3,7 +3,7 @@ import Topbar from "./Topbar"
 
 const Dashboard = () => {
     return (
-        <div className="flex-col bg-white">
+        <div className="flex-col bg-white text-red-200">
             <Topbar />
             <Home />
         </div>
